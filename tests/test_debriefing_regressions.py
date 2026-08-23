4b7fdb38145c68a00253f90652c55d9246c3a2fb
Alex Vera <alex.vera.contacto@gmail.com>
2026-08-24 01:35:39 +0200

    Fix loading legacy formation task objects

--- tests/test_debriefing_regressions.py
+++ tests/test_debriefing_regressions.py
@@ -1,5 +1,7 @@
 from types import SimpleNamespace
 
+from dcs.task import OptFormation
+
 from game.debriefing import FlyingUnitHitPointUpdate
 from game.flightplan.waypointoptions.formation import Formation
 
@@ -30,5 +32,34 @@ def test_formation_loads_from_legacy_task_dict() -> None:
         },
         "number": 1,
     }
+
+    assert Formation(legacy_value) is Formation.LINE_ABREAST_OPEN
+
+
+def test_formation_loads_from_legacy_task_dict_with_different_task_number() -> None:
+    legacy_value = {
+        "id": "WrappedAction",
+        "auto": False,
+        "enabled": True,
+        "params": {
+            "action": {
+                "id": "Option",
+                "params": {
+                    "name": 5,
+                    "value": 65538,
+                    "formationIndex": 1,
+                    "variantIndex": 2,
+                },
+            }
+        },
+        "number": 2,
+    }
+
+    assert Formation(legacy_value) is Formation.LINE_ABREAST_OPEN
+
+
+def test_formation_loads_from_legacy_task_object_with_different_task_number() -> None:
+    legacy_value = OptFormation.line_abreast_open()
+    legacy_value.number = 2
 
     assert Formation(legacy_value) is Formation.LINE_ABREAST_OPEN
